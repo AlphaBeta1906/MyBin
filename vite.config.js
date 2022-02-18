3056dfa6cf3ebf27bd9f7e4b80f9c3d935f6f01d
fariz <farizi1906@gmail.com>
2022-02-18 15:53:59 +0700

    trying to make pwa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,32 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), VitePWA({})]
+    plugins: [react(),
+        VitePWA({
+            includeAssets: ["src/favicon.svg", "robots.txt"],
+            manifest: {
+                name: "MyBin",
+                short_name: "MyBin",
+                description: "a simple pastebin clone to host snippets of code online",
+                theme_color: "#ffffff",
+                icons: [{
+                        src: "pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "any maskable",
+                    }
+                ]
+            }
+        })
+    ]
 })
